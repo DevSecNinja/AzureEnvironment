76e64695aac875ad87ff82d22452dbb1b8ccb618
Jean-Paul van Ravensberg <14926452+Cloudenius@users.noreply.github.com>
2020-10-22 14:06:02 +0000

    Update postCreateCommand to use new venv paths

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-## Prepare Python venv for Pulumi
-python -m venv ./platform/compute/pulumi/venv
-./platform/compute/pulumi/venv/bin/python -m pip install --upgrade pip setuptools wheel
-./platform/compute/pulumi/venv/bin/python -m pip install -r ./platform/compute/pulumi/requirements.txt
+# Prepare Python venv for Pulumi
+python -m venv ./.virtualenvs/compute
+./.virtualenvs/compute/bin/python -m pip install --upgrade pip setuptools wheel
+./.virtualenvs/compute/bin/python -m pip install -r ./platform/compute/pulumi/requirements.txt
 
-python -m venv ./platform/infrastructure/pulumi/venv
-./platform/infrastructure/pulumi/venv/bin/python -m pip install --upgrade pip setuptools wheel
-./platform/infrastructure/pulumi/venv/bin/python -m pip install -r ./platform/infrastructure/pulumi/requirements.txt
+python -m venv ./.virtualenvs/infrastructure
+./.virtualenvs/infrastructure/bin/python -m pip install --upgrade pip setuptools wheel
+./.virtualenvs/infrastructure/bin/python -m pip install -r ./platform/infrastructure/pulumi/requirements.txt
 
-python -m venv ./platform/applications/pulumi/venv
-./platform/applications/pulumi/venv/bin/python -m pip install --upgrade pip setuptools wheel
-./platform/applications/pulumi/venv/bin/python -m pip install -r ./platform/applications/pulumi/requirements.txt
+python -m venv ./.virtualenvs/applications
+./.virtualenvs/applications/bin/python -m pip install --upgrade pip setuptools wheel
+./.virtualenvs/applications/bin/python -m pip install -r ./platform/applications/pulumi/requirements.txt
